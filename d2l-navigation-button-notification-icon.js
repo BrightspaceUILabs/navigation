af3f7ec1c21d62c72c5125a94c29e4a315b6bace
Cheick Cisse <Cheick.Cisse@D2L.com>
2019-07-08 15:23:09 -0400

    US107607 Made notification dot a component
    US107607

--- d2l-navigation-button-notification-icon.js
+++ d2l-navigation-button-notification-icon.js
@@ -4,6 +4,7 @@ import 'd2l-icons/d2l-icon.js';
 import 'd2l-icons/tier2-icons.js';
 import 'd2l-button/d2l-button-behavior.js';
 import 'd2l-polymer-behaviors/d2l-focusable-behavior.js';
+import './d2l-navigation-notification-icon.js';
 
 import { PolymerElement, html } from '@polymer/polymer/polymer-element.js';
 import { mixinBehaviors } from '@polymer/polymer/lib/legacy/class.js';
@@ -24,6 +25,7 @@ class D2LNavigationButtonNotificationIcon extends mixinBehaviors([D2L.PolymerBeh
 			},
 			notification: {
 				type: Boolean,
+				value: false,
 				reflectToAttribute: true
 			},
 			text: {
@@ -36,44 +38,20 @@ class D2LNavigationButtonNotificationIcon extends mixinBehaviors([D2L.PolymerBeh
 	}
 	static get template() {
 		const template = html`
-		<style is="custom-style" include="d2l-offscreen-shared-styles">
+		<style is="custom-style">
 			:host {
 				display: inline-block;
 				height: 100%;
-			}
-			:host([notification]) .d2l-navigation-button-notification-indicator {
-				display: inline-block;
 			}
 			.d2l-navigation-button-icon-container {
 				display: inline-block;
 				position: relative;
 			}
-			.d2l-navigation-button-notification-indicator {
-				display: none;
-				height: 100%;
-				position: absolute;
-				right: calc(-50% - 4px);
-				top: calc(-50% + 11px);
-				width: 100%;
-			}
-			:host(:dir(rtl)) .d2l-navigation-button-notification-indicator {
-				left: calc(-50% - 4px);
-				right: auto;
-			}
-			.d2l-navigation-button-notification-indicator-icon {
-				background: var(--d2l-color-primary-accent-indicator);
-				border: 2px solid white;
-				height: 10px;
-				width: 10px;
-				border-radius: 50%;
-			}
 		</style>
 		<d2l-navigation-button aria-expanded$="[[ariaExpanded]]" aria-haspopup$="[[ariaHaspopup]]" aria-label$="[[ariaLabel]]" aria-describedby-text="[[_getNotificationText(notification, notificationText)]]" class="d2l-focusable" disabled$="[[disabled]]" autofocus$="[[autofocus]]" form$="[[form]]" formaction$="[[formaction]]" formenctype$="[[formenctype]]" formmethod$="[[formmethod]]" formnovalidate$="[[formnovalidate]]" formtarget$="[[formtarget]]" name$="[[name]]" text="[[text]]" type$="[[type]]">
 			<span class="d2l-navigation-button-icon-container">
 				<d2l-icon icon="[[icon]]" class="d2l-navigation-button-icon"></d2l-icon>
-				<span class="d2l-navigation-button-notification-indicator">
-					<div class="d2l-navigation-button-notification-indicator-icon"></div>
-				</span>
+				<d2l-navigation-notification-icon hidden$="[[!notification]]"></d2l-navigation-notification-icon>
 			</span>
 		</d2l-navigation-button>
 		`;
