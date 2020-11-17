0fd54b34396989c7731c059b7defcfa886a0f73c
Stacey Van Herk <Stacey.VanHerk@D2L.com>
2020-11-17 16:36:45 -0500

    Debugging

--- test/visual-diff/visual-diff.js
+++ test/visual-diff/visual-diff.js
@@ -159,6 +159,9 @@ class VisualDiff {
 		const currentFiles = this._fs.getCurrentFiles();
 		const goldenFiles = await this._fs.getGoldenFiles();
 
+		process.stdout.write(currentFiles);
+		process.stdout.write(goldenFiles);
+
 		for (let i = 0; i < goldenFiles.length; i++) {
 			const fileName = goldenFiles[i];
 			if (!currentFiles.includes(fileName)) {
