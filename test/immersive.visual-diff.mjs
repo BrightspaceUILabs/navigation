b37f062ac785e838767a7c86f722ce1c16f85d9a
Dave Lockhart <dlockhart@users.noreply.github.com>
2022-01-07 13:38:52 -0500

    visual-diff tests for all navigation components

---
+++ test/immersive.visual-diff.mjs
@@ -0,0 +1,115 @@
+/* eslint-disable no-invalid-this */
+/* global forceFocusVisible */
+import puppeteer from 'puppeteer';
+import { VisualDiff } from '@brightspace-ui/visual-diff';
+
+async function initTest(page, name) {
+	await page.$eval(`#${name}`, (template) => {
+		const clone = template.content.cloneNode(true);
+		document.body.insertBefore(clone, document.body.firstChild);
+	});
+	await page.waitForTimeout(100);
+}
+
+async function reset(page) {
+	await page.evaluate(() => {
+		const elem = document.querySelector('d2l-navigation-immersive');
+		elem.parentNode.removeChild(elem);
+	});
+}
+
+describe('d2l-navigation-immersive', function() {
+	const visualDiff = new VisualDiff('d2l-navigation-immersive', import.meta.url);
+
+	let browser, page;
+
+	before(async() => {
+		browser = await puppeteer.launch();
+		page = await visualDiff.createPage(browser);
+		await page.goto(
+			`${visualDiff.getBaseUrl()}/test/immersive.visual-diff.html`,
+			{waitUntil: ['networkidle0', 'load']}
+		);
+		await page.bringToFront();
+	});
+
+	after(async() => await browser.close());
+
+	[
+		'normal',
+		'fullscreen',
+		'both-slots',
+		'no-right-slot',
+		'no-middle-slot'
+	].forEach((name) => {
+
+		describe(name, () => {
+
+			before(async() => await initTest(page, name));
+
+			after(async() => await reset(page));
+
+			[1500, 929, 767].forEach((width) => {
+				it(`${width}`, async function() {
+					await page.setViewport({
+						height: 200,
+						width: width,
+						deviceScaleFactor: 2
+					});
+					await visualDiff.screenshotAndCompare(page, this.test.fullTitle());
+				});
+			});
+
+		});
+
+	});
+
+	describe('focus', () => {
+
+		before(async() => {
+			await page.setViewport({
+				height: 200,
+				width: 1000,
+				deviceScaleFactor: 2
+			});
+		});
+
+		afterEach(async() => reset(page));
+
+		it('back-button', async function() {
+			await initTest(page, 'normal');
+			await page.$eval('d2l-navigation-immersive', (elem) => forceFocusVisible(elem))
+			await visualDiff.screenshotAndCompare(page, this.test.fullTitle());
+		});
+
+	});
+
+	describe('new-slots', () => {
+
+		before(async() => {
+			await page.setViewport({
+				height: 200,
+				width: 1000,
+				deviceScaleFactor: 2
+			});
+		});
+
+		afterEach(async() => reset(page));
+
+		['right', 'middle'].forEach((slot) => {
+			it(`${slot}`, async function() {
+				await initTest(page, `no-${slot}-slot`);
+				await page.evaluate((slot) => {
+					const newContent = document.createElement('div');
+					newContent.setAttribute('slot', slot);
+					newContent.innerHTML = 'New Content';
+					document.querySelector('d2l-navigation-immersive').appendChild(newContent);
+				}, slot);
+				await page.waitForTimeout(100);
+				await visualDiff.screenshotAndCompare(page, this.test.fullTitle());
+			});
+		});
+
+	});
+
+});
---
+++ test/iterator.visual-diff.mjs
@@ -0,0 +1,83 @@
+/* eslint-disable no-invalid-this */
+/* global forceFocusVisible */
+import puppeteer from 'puppeteer';
+import { oneEvent, VisualDiff } from '@brightspace-ui/visual-diff';
+
+describe('d2l-navigation-iterator', function() {
+	const visualDiff = new VisualDiff('d2l-navigation-iterator', import.meta.url);
+
+	let browser, page;
+
+	before(async() => {
+		browser = await puppeteer.launch();
+		page = await visualDiff.createPage(browser);
+		await page.goto(
+			`${visualDiff.getBaseUrl()}/test/iterator.visual-diff.html`,
+			{waitUntil: ['networkidle0', 'load']}
+		);
+		await page.bringToFront();
+	});
+
+	beforeEach(async() => {
+		await visualDiff.resetFocus(page);
+	});
+
+	after(async() => await browser.close());
+
+	describe('item', () => {
+
+		[
+			{ category: 'previous', tests: ['normal', 'hover', 'focus'] },
+			{ category: 'next', tests: ['normal'] },
+			{ category: 'hide-text', tests: ['normal', 'hover', 'focus'] },
+			{ category: 'disabled', tests: ['normal', 'hover', 'focus'] },
+			{ category: 'custom-text', tests: ['normal'] }
+		].forEach((entry) => {
+			describe(entry.category, () => {
+				entry.tests.forEach((name) => {
+					it(name, async function() {
+						const selector = `#item-${entry.category}`;
+						const hoverSelector = `#item-${entry.category} > *`;
+
+						if (name === 'hover' || name === 'focus') {
+							if (entry.category.indexOf('disabled') > -1) {
+								if (name === 'hover') await page.hover(hoverSelector);
+								else if (name === 'focus') await page.$eval(selector, (elem) => forceFocusVisible(elem));
+							} else {
+								setTimeout(() => {
+									if (name === 'hover') page.hover(hoverSelector);
+									else if (name === 'focus') page.$eval(selector, (elem) => forceFocusVisible(elem));
+								});
+								await oneEvent(page, selector, 'd2l-tooltip-show');
+							}
+						}
+
+						const rect = await visualDiff.getRect(page, selector);
+						// without captureBeyondViewport: false, screenshotAndCompare() triggers a resize event
+						// which hides the tooltips
+						await visualDiff.screenshotAndCompare(page, this.test.fullTitle(), { captureBeyondViewport: false, clip: rect });
+					});
+				});
+			});
+		});
+
+	});
+
+	describe('iterator', () => {
+
+		[
+			'default',
+			'hide-text',
+			'disabled',
+			'custom-text',
+			'slot'
+		].forEach((name) => {
+			it(name, async function() {
+				const rect = await visualDiff.getRect(page, `#iterator-${name}`);
+				await visualDiff.screenshotAndCompare(page, this.test.fullTitle(), { clip: rect });
+			});
+		});
+
+	});
+
+});
