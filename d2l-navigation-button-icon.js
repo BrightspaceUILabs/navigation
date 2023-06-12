f42495c980228f3a7f5cc452facac6bcee0e770d
Derek Moran <dmoran@desire2learn.com>
2023-06-12 16:06:45 -0400

    DE51509 - add tooltip-location property to d2l-navigation-button-icon

--- d2l-navigation-button-icon.js
+++ d2l-navigation-button-icon.js
@@ -45,6 +45,11 @@ class NavigationButtonIcon extends FocusMixin(LitElement) {
 			 */
 			textHidden: { attribute: 'text-hidden', type: Boolean },
 			/**
+			 * Location of the tooltip
+			 * @type {'top'|'bottom'|'left'|'right'}
+			 */
+			tooltipLocation: { attribute: 'tooltip-location', type: String },
+			/**
 			 * Offset of the tooltip
 			 * @type {Number}
 			 */
@@ -71,6 +76,7 @@ class NavigationButtonIcon extends FocusMixin(LitElement) {
 		this.noHighlightBorder = false;
 		this.textHidden = false;
 		this._buttonId = getUniqueId();
+		this.tooltipLocation = 'bottom';
 	}
 
 	static get focusElementSelector() {
@@ -98,7 +104,7 @@ class NavigationButtonIcon extends FocusMixin(LitElement) {
 				ariaLabel: this.text,
 				id: this._buttonId,
 				text: nothing,
-				tooltip: html`<d2l-tooltip close-on-click for="${this._buttonId}" for-type="label" position="bottom" offset="${ifDefined(this.tooltipOffset)}">${this.text}</d2l-tooltip>`
+				tooltip: html`<d2l-tooltip close-on-click for="${this._buttonId}" for-type="label" position="${this.tooltipLocation}" offset="${ifDefined(this.tooltipOffset)}">${this.text}</d2l-tooltip>`
 			};
 		}
 		return {
