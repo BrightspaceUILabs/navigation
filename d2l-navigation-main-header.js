590974afee2b0e82f0971b9be0b8c22acf8719cf
Dave Lockhart <dlockhart@users.noreply.github.com>
2024-06-17 12:17:41 -0400

    feat: migrate some nav elements to Lit (#216)

--- d2l-navigation-main-header.js
+++ d2l-navigation-main-header.js
@@ -1,61 +1,57 @@
-import { html, PolymerElement } from '@polymer/polymer/polymer-element.js';
+import { css, html, LitElement } from 'lit';
 import { navigationSharedStyle } from './d2l-navigation-shared-styles.js';
 
-/**
-`d2l-navigation-main-header`
-Polymer-based web component for the larger, upper section of the navigational element
+class NavigationMainHeader extends LitElement {
 
-@demo demo/navigation-main-header.html
-*/
-class D2LNavigationMainHeader extends PolymerElement {
+	static get styles() {
+		return [navigationSharedStyle, css`
+			:host {
+				display: block;
+			}
 
-	static get template() {
-		const template = html`
-			${navigationSharedStyle}
-			<style>
-				:host {
-					display: block;
-				}
+			.d2l-navigation-header-container {
+				align-items: center;
+				display: flex;
+				height: 90px;
+			}
 
+			@media (max-width: 767px) {
 				.d2l-navigation-header-container {
-					align-items: center;
-					display: flex;
-					height: 90px;
+					height: 72px;
 				}
+			}
 
-				@media (max-width: 767px) {
-					.d2l-navigation-header-container {
-						height: 72px;
-					}
-				}
+			div ::slotted(.d2l-navigation-header-left),
+			div ::slotted(.d2l-navigation-header-right) {
+				align-items: center;
+				display: flex;
+				height: 100%;
+			}
 
-				div ::slotted(.d2l-navigation-header-left),
-				div ::slotted(.d2l-navigation-header-right) {
-					align-items: center;
-					display: flex;
-					height: 100%;
-				}
+			div ::slotted(.d2l-navigation-header-left) {
+				flex: 0 1 auto;
+			}
 
-				div ::slotted(.d2l-navigation-header-left) {
-					flex: 0 1 auto;
-				}
+			div ::slotted(.d2l-navigation-header-right) {
+				flex: 0 0 auto;
+			}
 
-				div ::slotted(.d2l-navigation-header-right) {
-					flex: 0 0 auto;
-				}
+			.d2l-navigation-gutter {
+				display: inline-block;
+				flex: 1 1 auto;
+				min-width: var(--d2l-navigation-margin-regular);
+			}
 
+			@media (max-width: 615px) {
 				.d2l-navigation-gutter {
-					display: inline-block;
-					flex: 1 1 auto;
-					min-width: var(--d2l-navigation-margin-regular);
+					min-width: var(--d2l-navigation-margin-regular) / 2;
 				}
+			}
+		`];
+	}
 
-				@media (max-width: 615px) {
-					.d2l-navigation-gutter {
-						min-width: var(--d2l-navigation-margin-regular) / 2;
-					}
-				}
-			</style>
+	render() {
+		return html`
 			<div class="d2l-navigation-centerer">
 				<div class="d2l-navigation-gutters">
 					<div class="d2l-navigation-header-container">
@@ -66,8 +62,6 @@ class D2LNavigationMainHeader extends PolymerElement {
 				</div>
 			</div>
 		`;
-		template.setAttribute('strip-whitespace', '');
-		return template;
 	}
 }
-customElements.define('d2l-navigation-main-header', D2LNavigationMainHeader);
+customElements.define('d2l-navigation-main-header', NavigationMainHeader);
